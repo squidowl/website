33a2fbb34e4c76d4dae276e00b292dc95bbc8bc3
Casper Rogild Storm <casper+github@rogildstorm.com>
2024-03-14 14:06:12 +0100

    svelte init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<section class="text-center w-full h-screen bg-gradient-to-b from-indigo to-red">
+	<div class="flex flex-col items-center w-full h-full p-8 gap-8">
+		<img width="200px" src="logo.png" alt="Halloy" />
+		<img width="700px" src="animation.gif" alt="Halloy" class="rounded-lg" />
+		<p>Cross-platform IRC client</p>
+		<a href="https://github.com/squidowl/halloy" target="_blank" class="rounded-button">GitHub</a>
+        
+	</div>
+</section>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        'indigo': '#1C2D75',
+        'red': '#ED7E5B',
+      },
+    },
+  },
+  plugins: [],
+}
+
